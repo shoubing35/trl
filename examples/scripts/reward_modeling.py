0c973b8ced5667c2cd76ac8967d8d9f052a9a8a4
Charles <shoubing@gmail.com>
2025-04-07 09:33:39 -0700

    print test and eval size

--- examples/scripts/reward_modeling.py
+++ examples/scripts/reward_modeling.py
@@ -115,19 +115,22 @@ if __name__ == "__main__":
     ##########
     # Training
     ##########
+
+    train_dataset = dataset[script_args.dataset_train_split] # charles
+    eval_dataset = dataset[script_args.dataset_train_split] # charles: dataset has no test split yet
+
+    print(f"Train dataset size: {len(train_dataset)}") # charles
+    print(f"Eval dataset size: {len(eval_dataset)}") # charles
+
     trainer = RewardTrainer(
         model=model,
         processing_class=tokenizer,
         args=training_args,
-        train_dataset=dataset[script_args.dataset_train_split],
-        eval_dataset=dataset[script_args.dataset_train_split], # charles: dataset has no test split yet
+        train_dataset=train_dataset, # charles
+        eval_dataset=eval_dataset, # charles
         # eval_dataset=dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None, # charles commented out
         peft_config=get_peft_config(model_args),
     )
-
-    print(f"Train dataset size: {len(train_dataset)}") # charles
-    print(f"Eval dataset size: {len(eval_dataset)}") # charles
-    print(f"Batch size: {training_args.per_device_train_batch_size}") # charles
 
     trainer.train()
 
