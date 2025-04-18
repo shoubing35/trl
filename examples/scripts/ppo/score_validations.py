bbb882f34b9dd26d4522df8caccd7cf8a161b3a0
Charles <shoubing@gmail.com>
2025-04-19 07:57:33 +0900

    a

--- examples/scripts/ppo/score_validations.py
+++ examples/scripts/ppo/score_validations.py
@@ -213,66 +213,14 @@ if __name__ == "__main__":
     # for i, input_ids in enumerate(inputs["input_ids"]): # debug: batch generate index out of range
     #     print(f"Padded input {i}: {len(input_ids)} tokens")
 
-    ################
-    # Generate completions before training
-    ################
-    # Craete fresh peft model (for loading in 8-bit)
-    from peft import get_peft_model
-    import torch
-    peft_base = get_peft_model(base_model, peft_config)
-    peft_base.eval()
-
-    predictions = []
-    for i in range(2):
-        inputs = tokenizer(
-            # df_text[0], # first question in dataset
-            # text_inference, # manual question
-            df_prompt[i],
-            return_tensors="pt",
-            padding=True,
-            truncation=True,
-            max_length=2048,
-        )
-        inputs.to(peft_base.device) # Create fresh peft model
-
-        output = peft_base.generate(
-            **inputs,
-            max_new_tokens=1024,
-            do_sample=False,
-            # temperature=0.7,
-            # num_return_sequences=2,
-        )
-
-        # Figure out how many tokens were used for the prompt:
-        prompt_length = inputs["input_ids"].shape[1]
-
-        # Decode only tokens beyond the prompt
-        # completions = []
-        # for output in outputs:
-        # Slice off the prompt tokens to keep only the model’s response
-        response_tokens = output[0][prompt_length:]
-        response_text = tokenizer.decode(response_tokens, skip_special_tokens=True)
-        # completions.append(response_text)
-        print("\nBase Model Inference:")
-        # for i, completion in enumerate(completions):  # Print completions and their scores
-        # print(f"\n--- Completion {i + 1} ---")
-        print(response_text)
-        prediction = extract_boxed(response_text)
-        predictions.append(prediction)
-        print(f"Prediction = {prediction}")
-        print(f"Answer = {df['answer'][i]}")
-    print(f"predictions = {predictions}")
-    print(f"answers = {df['answer']}")
-    score_predictions(predictions, df['answer'][:2], verbose=True)
-
     # ################
-    # # Generate completions after sft training
+    # # Generate completions before training
     # ################
-    # # Load sft-trained peft model
-    # from peft import PeftModel
-    # adapter_path = "/content/drive/MyDrive/Colab_Notebooks/llama-1B-sft"
-    # peft_sft = PeftModel.from_pretrained(base_model, adapter_path)  # Load peft model
-    # peft_sft.eval()
+    # # Craete fresh peft model (for loading in 8-bit)
+    # from peft import get_peft_model
+    # import torch
+    # peft_base = get_peft_model(base_model, peft_config)
+    # peft_base.eval()
     #
     # predictions = []
     # for i in range(2):
@@ -285,9 +233,9 @@ if __name__ == "__main__":
     #         truncation=True,
     #         max_length=2048,
     #     )
-    #     inputs.to(peft_sft.device)
+    #     inputs.to(peft_base.device) # Create fresh peft model
     #
-    #     output = peft_sft.generate(
+    #     output = peft_base.generate(
     #         **inputs,
     #         max_new_tokens=1024,
     #         do_sample=False,
@@ -301,11 +249,11 @@ if __name__ == "__main__":
     #     # Decode only tokens beyond the prompt
     #     # completions = []
     #     # for output in outputs:
-    #         # Slice off the prompt tokens to keep only the model’s response
+    #     # Slice off the prompt tokens to keep only the model’s response
     #     response_tokens = output[0][prompt_length:]
     #     response_text = tokenizer.decode(response_tokens, skip_special_tokens=True)
     #     # completions.append(response_text)
-    #     print("\nSFT Model Inference:")
+    #     print("\nBase Model Inference:")
     #     # for i, completion in enumerate(completions):  # Print completions and their scores
     #     # print(f"\n--- Completion {i + 1} ---")
     #     print(response_text)
@@ -316,6 +264,58 @@ if __name__ == "__main__":
     # print(f"predictions = {predictions}")
     # print(f"answers = {df['answer']}")
     # score_predictions(predictions, df['answer'][:2], verbose=True)
+
+    ################
+    # Generate completions after sft training
+    ################
+    # Load sft-trained peft model
+    from peft import PeftModel
+    adapter_path = "/content/drive/MyDrive/Colab_Notebooks/llama-1B-sft"
+    peft_sft = PeftModel.from_pretrained(base_model, adapter_path)  # Load peft model
+    peft_sft.eval()
+
+    predictions = []
+    for i in range(2):
+        inputs = tokenizer(
+            # df_text[0], # first question in dataset
+            # text_inference, # manual question
+            df_prompt[i],
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            max_length=2048,
+        )
+        inputs.to(peft_sft.device)
+
+        output = peft_sft.generate(
+            **inputs,
+            max_new_tokens=1024,
+            do_sample=False,
+            # temperature=0.7,
+            # num_return_sequences=2,
+        )
+
+        # Figure out how many tokens were used for the prompt:
+        prompt_length = inputs["input_ids"].shape[1]
+
+        # Decode only tokens beyond the prompt
+        # completions = []
+        # for output in outputs:
+            # Slice off the prompt tokens to keep only the model’s response
+        response_tokens = output[0][prompt_length:]
+        response_text = tokenizer.decode(response_tokens, skip_special_tokens=True)
+        # completions.append(response_text)
+        print("\nSFT Model Inference:")
+        # for i, completion in enumerate(completions):  # Print completions and their scores
+        # print(f"\n--- Completion {i + 1} ---")
+        print(response_text)
+        prediction = extract_boxed(response_text)
+        predictions.append(prediction)
+        print(f"Prediction = {prediction}")
+        print(f"Answer = {df['answer'][i]}")
+    print(f"predictions = {predictions}")
+    print(f"answers = {df['answer'][:len(predictions)]}")
+    score_predictions(predictions, df['answer'][:len(predictions)], verbose=True)
 
     # ################
     # # Generate completions after grpo training
