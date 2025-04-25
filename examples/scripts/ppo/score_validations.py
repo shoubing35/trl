267bbc311d217327a91a282cad1b197a0cee789c
Charles <shoubing@gmail.com>
2025-04-26 08:33:34 +0900

    a

--- examples/scripts/ppo/score_validations.py
+++ examples/scripts/ppo/score_validations.py
@@ -276,7 +276,7 @@ if __name__ == "__main__":
     peft_sft.eval()
 
     predictions = []
-    for i in range(8,9):
+    for i in range(8,10):
         inputs = tokenizer(
             # df_text[0], # first question in dataset
             # text_inference, # manual question
